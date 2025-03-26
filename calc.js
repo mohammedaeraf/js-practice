296e808b748cd9276454bc6884f471ff57135410
Mohammed Aeraf <aeraf@outlook.com>
2025-03-26 15:45:51 +0530

    Variables and Expressions

---
+++ calc.js
@@ -0,0 +1,10 @@
+let i = 10;
+let j = 20;
+let x = i + j;  // expression
+let y = j - i;
+let z = i * j;
+let n = j / i;
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(n);
---
+++ ifelse.js
@@ -0,0 +1,14 @@
+/*
+odd or even program
+writte in js
+*/
+let n = 7;
+let remainder = n % 2;
+// console.log(remainder);
+
+if(remainder == 0) {
+    console.log(n + " is an Even number");
+}
+else {
+    console.log(n + " is an Odd number");
+}
